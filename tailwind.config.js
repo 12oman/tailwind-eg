4fde6b71c139a7ec79902a3b5ec4693116fd3422
12oman <romanmitch@gmail.com>
2024-04-19 08:57:53 +1200

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // use the index.html, contact.html, examples.html as the content source of truth
+  content: ["./index.html","./contact.html", "./examples.html", "./blog.html", "./gallery.html", "./step.html" ],
+  theme: {
+    // colors: {
+    //   coffee: '#C0FFEE',
+    //   current: 'currentColor',
+    //   transparent: 'transparent',
+    // },
+
+    extend: {},
+  },
+  plugins: [],
+}
+
